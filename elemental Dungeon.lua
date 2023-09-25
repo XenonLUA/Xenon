c7122c3e1fabd3bcfd2a7e2917155e93a51d11be
yusup276 <49691279+yusup276@users.noreply.github.com>
2023-09-25 19:32:36 +0700

    Update Punch Simulator.lua

--- elemental Dungeon.lua
+++ elemental Dungeon.lua
@@ -1,187 +1,58 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "XenonHUB", HidePremium = false, SaveConfig = true, ConfigFolder = "XenonHUB"})
+
 -- VARIABLES
-repeat task.wait() until game:IsLoaded()
-getgenv().autofarm = nil
-getgenv().heal = nil
+getgenv().win = nil
+getgenv().punch = nil
 
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Services = ReplicatedStorage.ReplicatedStorage.Packages.Knit.Services
-local PartyService = Services.PartyService
-local GetPartyFromPlayer = PartyService.RF.GetPartyFromPlayer
-local VoteOn = PartyService.RF.VoteOn
-local StartDungeon = Services.DungeonService.RF.StartDungeon
+function win()
+while getgenv().win do
+local args = {
+    [1] = true
+}
 
-local UseSword = Services.WeaponService.RF.UseSword
-function createDungeon(name, difficulty, join, hardcore)
-    CreateParty:InvokeServer(name, difficulty, join, not hardcore and "Normal")
-    StartParty:InvokeServer(GetPartyFromPlayer:InvokeServer(LocalPlayer)['Data']['UUID'])
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("PushEvent"):FireServer(unpack(args))
+wait()
+end
 end
 
-function heal()
-  while getgenv().heal do
-  game:GetService("ReplicatedStorage").ReplicatedStorage.Packages.Knit.Services.HealingService.RF.UseHeal:InvokeServer()
-  wait(5)
-  end
-  end
-
-
-
-
--- UI LIBRARY
-
-local Library = loadstring(game:HttpGet("https:/raw.githubusercontent.com/Attrixx/Scandia/main/KavoUiLib.lua"))() -- GETS THE UI LIBRARY
-local Window = Library.CreateLib("Elemental Dungeon", "Sentinel") -- CREATES THE WINDOW
-
--- MAIN
-local Main = Window:NewTab("Main") -- CREATES THE MAIN TAB
-local MobFarmSection = Main:NewSection("Auto Dungeon") -- CREATES THE MOB FARM SECTION
-
-MobFarmSection:NewToggle("Start Auto Dungeon", "Toggles the autofarming of the mobs", function(v) -- CREATES THE START / STOP TOGGLE
-    getgenv().autofarm = v
-    repeat task.wait() until game:IsLoaded()
-    function retry()
-    VoteOn:InvokeServer("Retry")
+function punch()
+while getgenv().punch do
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("DamageIncreaseOnClickEvent"):FireServer()
+wait(.000000000)
+end
 end
 
-function goNext()
-    VoteOn:InvokeServer("Next")
-end
 
-if game.PlaceId == 10515146389 then
-    createDungeon("SnowCastle", "Hell", "All", "Normal")
-end
-function startDungeon()
-    StartDungeon:InvokeServer()
-end
+--Tabs
+local XenTab = Window:MakeTab({
+	Name = "Menu",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
 
-function getAliveMobs()
-    local mobs = {}
-    for _,v in next, workspace.Mobs:GetChildren() do
-        if v:FindFirstChild("Humanoid") and v.PrimaryPart and v.Humanoid.Health > 0 then
-            table.insert(mobs, v)
-        end
-    end
-    return mobs
-end
+XenTab:AddToggle({
+	Name = "Auto Win Nearest",
+	Default = false,
+	Callback = function(Value)
+	getgenv().win = Value
+	win()
+	end    
+})
 
-function swing()
-    UseSword:InvokeServer()
-end
+XenTab:AddToggle({
+	Name = "Auto Punch",
+	Default = false,
+	Callback = function(Value)
+	getgenv().punch = Value
+	punch()
+	end    
+})
 
--- THIS IS FOR DODGE ATTACKS, DOESN'T WORK YET. FEEL FREE TO MAKE IT WORK
 
-function isPositionClear(position, obstacles)
-    for _, obstacle in pairs(obstacles) do
-        if obstacle.Parent and obstacle.Name == "Warning" and obstacle.Transparency ~= 1 then
-            local distance = (Vector3.new(position.X, 0, position.Z) - Vector3.new(obstacle.Position.X, 0, obstacle.Position.Y)).Magnitude
-            if distance - 1 < (obstacle.Size.Magnitude / 2) then
-                return false
-            end
-        end
-    end
-    return true
-end
 
-function dogeAttacks(targetPosition, obstacles)
-    for pos1 = 1,100 do
-        for neg1 = -1, 1, 2 do
-            for pos2 = 1,100 do
-                for neg2 = -1, 1, 2 do 
-                    local randomOffset = Vector3.new(pos1 * neg1, 0, pos2 * neg2)
-                    local newPosition = targetPosition + randomOffset
 
-                    if isPositionClear(newPosition, obstacles) then
-                        return newPosition
-                    end
-                end
-            end
-        end
-    end
 
-    return nil
-end
 
-startDungeon()
 
-local obstacles = {}
-workspace.Effects.ChildAdded:Connect(function(child)
-    wait()
-    if child:IsA("BasePart") then
-        table.insert(obstacles, child)
-    else
-        for _,child in next, child:GetChildren() do
-            if child:IsA("BasePart") then
-                task.spawn(function()
-                    table.insert(obstacles, child)
-                end)
-            end
-        end
-    end
-end)
-
-workspace.Effects.ChildRemoved:Connect(function(child)
-    if child:IsA("BasePart") then
-        table.remove(obstacles, table.find(obstacles, child))
-    else
-        for _,child in next, child:GetChildren() do
-            if child:IsA("BasePart") then
-                table.remove(obstacles, table.find(obstacles, child))
-            end
-        end
-    end
-end)
-
-task.spawn(function()
-    while task.wait() and getgenv().autofarm do
-        if LocalPlayer.Character and LocalPlayer.Character.PrimaryPart then
-            LocalPlayer.Character.PrimaryPart.Velocity = Vector3.new(0,0,0)
-        end
-    end
-end)
-
-function killMob(mob)
-    task.wait()
-    if LocalPlayer.Character and LocalPlayer.Character.PrimaryPart and mob.PrimaryPart and mob:FindFirstChild("Humanoid") and mob.Humanoid.Health > 0 then
-
-        local targetPosition = (mob.PrimaryPart.CFrame).p + Vector3.new(0,4,0)
-        if not targetPosition then
-            LocalPlayer.Character.PrimaryPart.CFrame = CFrame.new(1000,100,1000)
-        else
-            local oldHealth = mob.Humanoid.Health
-            repeat
-                LocalPlayer.Character.PrimaryPart.CFrame = CFrame.new(targetPosition, mob.PrimaryPart.Position)
-                task.wait()
-                swing()
-            until not LocalPlayer.Character or not LocalPlayer.Character.PrimaryPart or mob.Humanoid.Health ~= oldHealth or #mob.Humanoid:GetPlayingAnimationTracks() > 1
-            if LocalPlayer.Character and LocalPlayer.Character.PrimaryPart then
-                LocalPlayer.Character.PrimaryPart.CFrame = LocalPlayer.Character.PrimaryPart.CFrame + Vector3.new(0,50,0)
-            end
-        end
-    end
-
-end
-
-function pickupShit()
-    for _,v in next, workspace.Camera.Drops:GetChildren() do
-        if v:FindFirstChild("Center") and v.Center:FindFirstChild("ProximityPrompt") then
-            fireproximityprompt(v.Center.ProximityPrompt)
-        end
-    end 
-end
-
-while getgenv().autofarm and task.wait() do
-    for _, mob in next, getAliveMobs() do
-        killMob(mob)
-        pickupShit()
-    end
-    if game:GetService("Players").LocalPlayer.PlayerGui.DungeonComplete.Main.Visible then
-        retry()
-    end
-end
-end)
-
-MobFarmSection:NewToggle(" Auto Heal", "Toggles the auto heal", function(v)
-getgenv().heal = v
-heal()
-end)
+OrionLib:Init()
