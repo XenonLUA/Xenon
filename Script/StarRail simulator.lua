5306ad39755b8e0731014bbcae115de98d3668ed
Xenon <49691279+XenonLUA@users.noreply.github.com>
2024-03-02 06:06:58 +0700

    Update StarRail simulator.lua

--- Script/StarRail simulator.lua
+++ Script/StarRail simulator.lua
@@ -1,13 +1,14 @@
 local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
-	
-	local PhantomForcesWindow = Library:NewWindow("StarRail Sim")
-	local x = PhantomForcesWindow:NewSection("Main")
-	
+
+        local PhantomForcesWindow = Library:NewWindow("StarRail Sim")
+        local x = PhantomForcesWindow:NewSection("Main")
+
 --Values
 _G.pet = false
 getgenv().TPPlace = "string"
 
 getgenv().potion = false
+getgenv().slot = false
 --function
 
 function Teleport(teleportPlace)
@@ -29,7 +30,7 @@ end
 function pet()
     while _G.pet do task.wait()
         game:GetService("ReplicatedStorage").Events.Hero.HeroAbility:FireServer(workspace.UserPets["1116656080"]["Pom-Pom"],"05-Emit")
-	    
+
 game:GetService("ReplicatedStorage").Events.Hero.HeroAttack:FireServer(workspace.UserPets["1116656080"]["Pom-Pom"])
 
 local args = {
@@ -75,11 +76,11 @@ game:GetService("ReplicatedStorage").Events.Hero.HeroAbility:FireServer(unpack(a
 end
 
     --Main
-	x:CreateToggle("Claim Potion", function(value)
+        x:CreateToggle("Claim Potion", function(value)
         getgenv().potion = value
         potion()
-		end)
-	
+                end)
+
 local slot = {
     "Slot001",
     "Slot002",
@@ -88,22 +89,24 @@ local slot = {
     "Slot005",
 }
 
-	x:CreateToggle("Auto SlotMachine", function(Value)
-	slot = Value 
-	while slot do task.wait(10)
-	  pcall(function()
+function slots()
+  while getgenv().slot do task.wait()
         for i,v in (slot) do
         game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Stats"):WaitForChild("ClaimSlot"):InvokeServer(v)
-            end
-	    end)
-	  end
-	end)
-	
-	--[[x:CreateToggle("Auto Collect", function(Value)
-	c = Value 
-	while c do
-	  pcall(function() 
-	    local playerhead = game.Players.LocalPlayer.Character.Head
+        end
+    end
+ end
+
+        x:CreateToggle("Auto SlotMachine", function(Value)
+        getgenv().slot = Value 
+        slots()
+        end)
+
+        --[[x:CreateToggle("Auto Collect", function(Value)
+        c = Value 
+        while c do
+          pcall(function() 
+            local playerhead = game.Players.LocalPlayer.Character.Head
 for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
   if v.Name == "TouchInterest" and v.Parent then
     firetouchinterest(playerhead, v.Parent, 0)
@@ -113,19 +116,19 @@ for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
     end
 end
 end)
-	end
+        end
   wait()
-	end)]]
-	
-	x:CreateToggle("Auto Power", function(Value)
-	a = Value 
-	while a do task.wait()
-	  pcall(function()
+        end)]]
+
+        x:CreateToggle("Auto Power", function(Value)
+        a = Value 
+        while a do task.wait()
+          pcall(function()
         game:GetService("ReplicatedStorage").Events.Fight.ClickDamage:FireServer()
-	    end)
-	  end
-	end)
-    
+            end)
+          end
+        end)
+
     x:CreateToggle("Auto Ability Pet", function(value)
         _G.pet = value
         pet()
